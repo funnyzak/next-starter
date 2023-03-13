6a3179f39dca5f5890df59a0d433711b1d6d724d
leon <silenceace@gmail.com>
2023-03-13 11:46:56 +0800

    feat: utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,7 @@
 export const isDev = process.env.NODE_ENV === 'development'
+
+export const isProd = process.env.NODE_ENV === 'production'
+
+export const HOST_URL = isDev
+  ? 'http://0.0.0.0:3000'
+  : process.env.HOST_URL
