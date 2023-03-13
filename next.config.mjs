c1b402ebd79a0a439d32e8c344cc9b6b01bded1a
leon <silenceace@gmail.com>
2023-03-13 16:43:20 +0800

    feat: next  config

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   experimental: {
     appDir: false,
   },
 
-  // safe config
   poweredByHeader: false,
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        // {
+        //   key: 'Referrer-Policy',
+        //   value: 'same-origin',
+        // },
+      ],
+    },
+  ],
 }
 
 export default nextConfig
